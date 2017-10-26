ce6e5e6dfb74c334156dca62b0add6c54ec6ac18
Eduardo G <egrueda@gmail.com>
2017-10-26 16:53:20 +0200

    Importar configuracion de Kodi en Sickrage

--- sickrage-osmc-install.sh
+++ sickrage-osmc-install.sh
@@ -2,9 +2,45 @@
 
 echo ""
 echo "#################################"
-echo "## OSMC Perfect-Pi :: Sickrage ##"
+echo "## OSMC Perfect-Pi (egrueda)   ##"
+echo "## Instalador de Sickrage      ##"
 echo "#################################"
 echo ""
+
+# Configuracion
+DOWNLOADS_PATH="/home/osmc/Downloads"
+MOVIES_PATH="/home/osmc/Movies"
+TVSHOWS_PATH="/home/osmc/TV Shows"
+KODI_CONFIG_FILE="/home/osmc/.kodi/userdata/guisettings.xml"
+TRANSMISSION_CONFIG_FILE="/home/osmc/.config/transmission-daemon/settings.json"
+SICKRAGE_CONFIG_FILE="/opt/sickrage/config.ini"
+SICKRAGE_USERNAME="osmc"
+SICKRAGE_PASSWORD="osmc"
+
+# Detectar OSMC
+if [ -f /etc/os-release ];
+then 
+	. /etc/os-release
+	if [ "$NAME" == "OSMC" ];
+	then
+		echo "Detectado $PRETTY_NAME $VERSION_ID";
+	else
+		echo "No se ha detectado la versión de OSMC. Abortando"
+		exit 1
+	fi
+else
+	echo "No se ha detectado OSMC. Abortando"
+	exit 1
+fi
+
+# Confirmacion
+echo "Estás a punto de instalar Sickrage en este dispositivo"
+read -r -p "${1:-¿Deseas continuar con la instalación? [s/N]} " response
+if [ "$response" != "s" ] && [ "$response" != "y" ];
+then
+	echo "Instalación abortada";
+	exit 0
+fi
 
 # Instalar GIT
 echo "[1] Instalación de Sickrage";
@@ -13,18 +49,18 @@ echo "[1.1] Comprobando GIT";
 HAS_GIT=$?
 if [ $HAS_GIT -eq "1" ];
 then
-	echo "Es necesario instalar GIT"
+	echo "No se encuentra el comando 'git'"
 	echo "Instalando mediante apt-get . . ."
 	sudo /usr/bin/apt-get -qq -y install git > /dev/null 2>&1
 	GIT_INSTALLED=$?
 	if [ $GIT_INSTALLED -eq "1" ];
 	then
-		echo "No se ha podido instalar GIT. Abortando"
+		echo "No se ha podido instalar 'git'. Abortando"
 		exit 1
 	fi
 
 else
-	echo "Se ha detectado GIT instalado"
+	echo "El comando 'git' ya se encuentra instalado"
 fi
 
 # Clonando GIT
@@ -75,11 +111,11 @@ then
 	exit 1
 else
 	echo "Script creado correctamente"
+	sudo /bin/systemctl daemon-reload
 fi
 
 # Iniciando Sickrage
 echo "[1.5] Iniciando Sickrage en el puerto 8081 (puede tardar un rato)";
-sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl restart sickrage
 sudo /bin/systemctl status sickrage
 if [ $? -ne "0" ];
@@ -101,42 +137,54 @@ echo -e "SickRage\nsickrage.service" | sudo tee /etc/osmc/apps.d/sickrage-app-os
 # config.ini
 echo "[1.8] Configurando Sickrate (config.ini)";
 /bin/mkdir /home/osmc/Downloads > /dev/null 2>&1
-/bin/cp /opt/sickrage/config.ini /opt/sickrage/config.ini.bak
+/bin/cp $SICKRAGE_CONFIG_FILE $SICKRAGE_CONFIG_FILE.bak
+sudo /bin/systemctl stop sickrage
 # General
-/bin/sed -i 's/auto_update = 0/auto_update = 1/g' /opt/sickrage/config.ini
-/bin/sed -i 's/indexerDefaultLang = en/indexerDefaultLang = es/g' /opt/sickrage/config.ini
-/bin/sed -i 's/web_username = ""/web_username = osmc/g' /opt/sickrage/config.ini
-/bin/sed -i 's/web_password = ""/web_password = osmc/g' /opt/sickrage/config.ini
-/bin/sed -i 's/home_layout = poster/home_layout = banner/g' /opt/sickrage/config.ini
+/bin/sed -i 's/auto_update = 0/auto_update = 1/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/indexerDefaultLang = en/indexerDefaultLang = es/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/web_username = ""/web_username = '$SICKRAGE_USERNAME'/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/web_password = ""/web_password = '$SICKRAGE_PASSWORD'/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/home_layout = poster/home_layout = banner/g' $SICKRAGE_CONFIG_FILE
 # Habilitar NewPCT
-/bin/sed -i "s/newpct = 0/newpct = 1/g" /opt/sickrage/config.ini
-/bin/sed -i 's/provider_order = ""/provider_order = newpct horriblesubs skytorrents hounddawgs hdbits abnormal alpharatio scenetime btn filelist rarbg hdspace torrentbytes torrentz elitetorrent speedcd tntvillage xthor shazbat_tv torrentproject immortalseed ncore_cc ilcorsaronero morethantv tvchaosuk hdtorrents_it iptorrents danishbits nyaa torrentday pretome cpasbien sceneaccess tokyotoshokan hdtorrents thepiratebay torrent9 bitcannon nebulance hd4free t411 torrentleech limetorrents norbits gftracker archetorrent/g' /opt/sickrage/config.ini
+/bin/sed -i "s/newpct = 0/newpct = 1/g" $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/provider_order = ""/provider_order = newpct horriblesubs skytorrents hounddawgs hdbits abnormal alpharatio scenetime btn filelist rarbg hdspace torrentbytes torrentz elitetorrent speedcd tntvillage xthor shazbat_tv torrentproject immortalseed ncore_cc ilcorsaronero morethantv tvchaosuk hdtorrents_it iptorrents danishbits nyaa torrentday pretome cpasbien sceneaccess tokyotoshokan hdtorrents thepiratebay torrent9 bitcannon nebulance hd4free t411 torrentleech limetorrents norbits gftracker archetorrent/g' $SICKRAGE_CONFIG_FILE
 # Formato de hora
-/bin/sed -i 's/time_preset = %I:%M:%S %p/time_preset = %H:%M:%S/g' /opt/sickrage/config.ini
-# Conectar con Transmission
-/bin/sed -i 's/torrent_method = blackhole/torrent_method = transmission/g' /opt/sickrage/config.ini
-/bin/sed -i 's/torrent_host = ""/torrent_host = http:\/\/localhost:9091\//g' /opt/sickrage/config.ini
-/bin/sed -i 's/torrent_username = ""/torrent_username = osmc/g' /opt/sickrage/config.ini
-/bin/sed -i 's/torrent_password = ""/torrent_password = osmc/g' /opt/sickrage/config.ini
-# Conectar con Kodi
-/bin/sed -i 's/use_kodi = 0/use_kodi = 1/g' /opt/sickrage/config.ini
-/bin/sed -i 's/kodi_host = ""/kodi_host = 127.0.0.1:8080/g' /opt/sickrage/config.ini
-/bin/sed -i 's/kodi_username = ""/kodi_username = osmc/g' /opt/sickrage/config.ini
-/bin/sed -i 's/kodi_password = ""/kodi_password = osmc/g' /opt/sickrage/config.ini
-/bin/sed -i 's/kodi_update_library = 0/kodi_update_library = 1/g' /opt/sickrage/config.ini
+/bin/sed -i 's/time_preset = %I:%M:%S %p/time_preset = %H:%M:%S/g' $SICKRAGE_CONFIG_FILE
 # Post processing
-/bin/sed -i 's/root_dirs = ""/root_dirs = 0|\/home\/osmc\/TV Shows/g' /opt/sickrage/config.ini
-/bin/sed -i 's/process_automatically = 0/process_automatically = 1/g' /opt/sickrage/config.ini
-/bin/sed -i 's/process_method = copy/process_method = symlink/g' /opt/sickrage/config.ini
-/bin/sed -i 's/tv_download_dir = ""/tv_download_dir = \/home\/osmc\/Downloads/g' /opt/sickrage/config.ini
-/bin/sed -i 's/naming_pattern = Season %0S\/%SN - S%0SE%0E - %EN/naming_pattern = Season %0S\/%SN.%0Sx%0E.%EN/g' /opt/sickrage/config.ini
+/bin/sed -i 's/root_dirs = ""/root_dirs = 0|\/home\/osmc\/TV Shows/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/process_automatically = 0/process_automatically = 1/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/process_method = copy/process_method = symlink/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/tv_download_dir = ""/tv_download_dir = \/home\/osmc\/Downloads/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/naming_pattern = Season %0S\/%SN - S%0SE%0E - %EN/naming_pattern = Season %0S\/%SN.%0Sx%0E.%EN/g' $SICKRAGE_CONFIG_FILE
 # Subtitulos
-/bin/sed -i 's/use_subtitles = 0/use_subtitles = 1/g' /opt/sickrage/config.ini
-/bin/sed -i 's/subtitles_languages = ""/subtitles_languages = spa/g' /opt/sickrage/config.ini
-/bin/sed -i 's/SUBTITLES_SERVICES_LIST = ""/SUBTITLES_SERVICES_LIST = "addic7ed,legendastv,opensubtitles,podnapisi,shooter,thesubdb,tvsubtitles,itasa,subscenter"/g' /opt/sickrage/config.ini
-/bin/sed -i 's/SUBTITLES_SERVICES_ENABLED = ""/SUBTITLES_SERVICES_ENABLED = 0|0|1|0|0|0|0|0|0/g' /opt/sickrage/config.ini
-echo "Sickrage configurado. Reiniciando . . ."
-sudo /bin/systemctl restart sickrage
+/bin/sed -i 's/use_subtitles = 0/use_subtitles = 1/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/subtitles_languages = ""/subtitles_languages = spa/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/SUBTITLES_SERVICES_LIST = ""/SUBTITLES_SERVICES_LIST = "addic7ed,legendastv,opensubtitles,podnapisi,shooter,thesubdb,tvsubtitles,itasa,subscenter"/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/SUBTITLES_SERVICES_ENABLED = ""/SUBTITLES_SERVICES_ENABLED = 0|0|1|0|0|0|0|0|0/g' $SICKRAGE_CONFIG_FILE
+
+# Conectar con Kodi
+echo "[1.9] Integración con Kodi";
+KODIPORT=`grep webserverport $KODI_CONFIG_FILE | cut -d '>' -f 2 | cut -d '<' -f 1`
+KODIUSER=`grep webserverusername $KODI_CONFIG_FILE | cut -d '>' -f 2 | cut -d '<' -f 1`
+KODIPASS=`grep webserverpassword $KODI_CONFIG_FILE | cut -d '>' -f 2 | cut -d '<' -f 1`
+/bin/sed -i 's/use_kodi = 0/use_kodi = 1/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/kodi_host = ""/kodi_host = 127.0.0.1:'$KODIPORT'/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/kodi_username = ""/kodi_username = '$KODIUSER'/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/kodi_password = ""/kodi_password = '$KODIPASS'/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/kodi_update_library = 0/kodi_update_library = 1/g' $SICKRAGE_CONFIG_FILE
+
+# Conectar con Transmission
+echo "[1.10] Integración con Transmission";
+#TORRENTPORT=`grep rpc-port $TRANSMISSION_CONFIG_FILE | tr -d ' ' | tr -d ',' | cut -d ':' -f 2`
+#TORRENTUSER=`grep rpc-username $TRANSMISSION_CONFIG_FILE | tr -d ' ' | tr -d ',' | tr -d '"'| cut -d ':' -f 2`
+#TORRENTPASS=`grep rpc-password $TRANSMISSION_CONFIG_FILE | tr -d ' ' | tr -d ',' | tr -d '"'| cut -d ':' -f 2`
+/bin/sed -i 's/torrent_method = blackhole/torrent_method = transmission/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/torrent_host = ""/torrent_host = http:\/\/localhost:9091\//g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/torrent_username = ""/torrent_username = osmc/g' $SICKRAGE_CONFIG_FILE
+/bin/sed -i 's/torrent_password = ""/torrent_password = osmc/g' $SICKRAGE_CONFIG_FILE
+
+echo "Sickrage configurado. Aplicando los cambios . . ."
+sudo /bin/systemctl start sickrage
 
 echo ""
 echo "Sickrage instalado correctamente"
